d4f75429a5424dd4882d332d68317fe45f0ef338
pavan kumar <pavankumar@scmgalaxy.com>
2020-03-30 18:00:52 -0400

    Init 3 projects to build microservice API

---
+++ moview-info-service/src/main/java/io/javabrains/moviewinfoservice/MoviewInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains.moviewinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviewInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviewInfoServiceApplication.class, args);
+	}
+
+}
