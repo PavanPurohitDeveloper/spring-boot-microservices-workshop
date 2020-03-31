de83eb3e01dfd57ca27fe947e37afc0de4f00717
pavan kumar <pavankumar@scmgalaxy.com>
2020-03-31 17:09:42 -0400

    Using a Bean to create RestTemplate instance

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/controller/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/controller/MovieCatalogResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,17 +19,21 @@ import io.javabrains.moviecatalogservice.model.Rating;
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-		RestTemplate restTemplate = new RestTemplate();
+		//RestTemplate restTemplate = new RestTemplate();
 
 		// get all rated move IDs 
 
 		// For each movie Id, call movie info service and get details
 		List<Rating> ratings = Arrays.asList(new Rating("1234", 4), new Rating("5678", 3));
 
+		//for each Movie ratings, get the Moview Info details
 		return ratings.stream().map( rating -> {
 				Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
 				return new CatalogItem(movie.getMovieName(), movie.getMoviewDescription(), rating.getRating());
